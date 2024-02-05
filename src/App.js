7f25d51152c964f27634956ed579b616e95ab6f6
Izack Lesher <msher3@gmail.com>
2024-02-05 10:28:41 +0200

    finish display table from rest api data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React from "react";
+import {
+  Button,
+  Alert,
+  Accordion,
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+} from "react-bootstrap";
+import UserList from "./components/UserList";
 
-import './App.css';
+import "./App.css";
 
-const  App = ()=> {
+const App = () => {
   return (
     <>
-      <p>beginning</p>
+      <Navbar expand="sm" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <UserList />;
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { UserService } from "../services/UserService";
+import { Container, Row, Col, Table } from "react-bootstrap";
+
+const UserList = () => {
+  const [state, setState] = useState({
+    users: [],
+  });
+
+  useEffect(() => {
+    UserService.getAllUsers()
+      .then((response) => {
+        setState({
+          ...state,
+          users: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      User List
+      <pre>{JSON.stringify(state.users)}</pre>
+      <pre>{JSON.stringify(state.users,null,2)}</pre>
+      <Container className="mt-3">
+        <Row>
+          <Col>
+            <h3 className="text-primary">User List</h3>
+            <p className="fst-italic">oioioi</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Table striped border hover className="shadow-lg text-center">
+              <thead>
+                <tr>
+                  <th>SNO</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Website</th>
+                  <th>Company</th>
+                  <th>Location</th>
+                </tr>
+              </thead>
+              <tbody>
+                {state.users.length > 0 &&
+                  state.users.map((user) => {
+                    return (
+                      <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.website}</td>
+                        <td>{user.company.name}</td>
+                        <td>{user.address.city}</td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default UserList;
