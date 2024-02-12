3b91e205a69fd5e555d3b142361d6c7d2b76cbae
Izack Lesher <msher3@gmail.com>
2024-02-12 09:58:50 +0200

    header line works with map

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,13 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useLayoutEffect} from "react";
 import { useState } from "react";
 import { UserService } from "../services/UserService";
 import { Container, Row, Col, Table } from "react-bootstrap";
 
 const UserList = () => {
   const [state, setState] = useState({
+    crmId:{},
+    crmName:{},
     users: [],
+    formFields:[],
+    phonePositionIndex:0,
+    formFieldsContet:[]
   });
+ // crmId: crm_id,
+ // crmName: crm_name,
+//  jsonData: data,
+///  formFields: Object.keys(data[0]),
+//  phonePositionIndex: Object.keys(data[0]).indexOf("phone"),
+//  formFieldsContet: data.map((obj) => {
 
+
+  /*
   useEffect(() => {
     UserService.getAllUsers()
       .then((response) => {
@@ -20,11 +33,62 @@ const UserList = () => {
         console.log(error);
       });
   }, [state]);
+*/
+  useLayoutEffect(() => {
+    const api_url = window.location.href;
+    fetch("StudentsData.json", {
+      method: "GET",
+      mode: "no-cors",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log("response.url= " + response.url);
+        return response.json();
+      })
+      .then((data) => {
+        // Save crm-id and crm-name
+        const crm_id = data[0].form_name_id;
+        const crm_name = data[0].form_name;
+        // remove first 2 properties from data[0] crm_id and crm_name from data
+        delete data[0].form_name_id;
+        delete data[0].form_name;
+        
+        //data.shift();
+        console.log("data neto follow= " + data);
+        console.log(JSON.stringify(data, null, 2));
+        setState({
+          crmId: crm_id,
+          crmName: crm_name,
+          users: data,
+          formFields: Object.keys(data[0]),
+          phonePositionIndex: Object.keys(data[0]).indexOf("phone"),
+          formFieldsContet: data.map((obj) => {
+            return Object.values(obj);
+          }),
+        });
+        console.log("data[0] = " + data[0]);
+        let fields = Object.keys(data[0]);
+        console.log(
+          "phonePositionIndex = " + Object.keys(data[0]).indexOf("phone")
+        );
+        console.log("data = " + data);
+        console.log("fields =" + fields);
+        console.log("keys = " + state.keys);
+        //console.log("values = " + values);
+        return data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
       User List
-     {/*} <pre>{JSON.stringify(state.users)}</pre>
+      {/*} <pre>{JSON.stringify(state.users)}</pre>
       <pre>{JSON.stringify(state.users,null,2)}</pre>*/}
       <Container className="mt-3">
         <Row>
@@ -37,7 +101,7 @@ const UserList = () => {
           <Col>
             <Table striped border hover className="shadow-lg text-center">
               <thead>
-                <tr>
+               {/*} <tr>
                   <th>SNO</th>
                   <th>Name</th>
                   <th>Email</th>
@@ -45,21 +109,33 @@ const UserList = () => {
                   <th>Company</th>
                   <th>Location</th>
                 </tr>
+               */}
+               <tr>
+                
+                {state.formFields.map((field)=>{
+                    return(
+                      <th>{field}</th>
+                    )
+                })}
+               </tr>
               </thead>
               <tbody>
-                {state.users.length > 0 &&
+                {/*{state.users.length > 0 &&
                   state.users.map((user) => {
                     return (
                       <tr key={user.id}>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.website}</td>
-                        <td>{user.company.name}</td>
-                        <td>{user.address.city}</td>
+                        <td>{state.user.id}</td>
+                        <td>{state.user.name}</td>
+                        <td>{state.user.email}</td>
+                        <td>{state.user.phone}</td>
+                        <td>{state.user.phone}</td>
                       </tr>
                     );
                   })}
+                  */}
+                  <tr>
+                    <td>{state.users.name}</td>
+                  </tr>
               </tbody>
             </Table>
           </Col>
