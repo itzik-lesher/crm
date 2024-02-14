e7fa36de62652031a0bc1389e36685fdb6446566
Izack Lesher <msher3@gmail.com>
2024-02-14 12:18:06 +0200

    error

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -118,6 +118,7 @@ const UserList = () => {
             <p className="fst-italic">oioioi</p>
           </Col>
         </Row>
+        
         <Row>
           <Col>
             <Table striped border hover className="shadow-lg text-center">
